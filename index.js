dd5d3cd1abe78e7ab2440f4e941acbc2dafcd8a2
ferdouszihad <f.zihad333@gmail.com>
2025-11-12 19:55:28 +0600

    first commit

--- index.js
+++ index.js
@@ -20,60 +20,57 @@ const client = new MongoClient(uri, {
   },
 });
 
-//server-Main
-async function run() {
+//listeners
+client
+  .connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Hero Apps Server listening ${port}`);
+      console.log(`Hero Apps Server Connected with DB`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//DB & collections
+const database = client.db("heroAppsDB");
+const appsCollection = database.collection("apps");
+
+//Apps Route
+
+app.get("/apps", async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db("heroAppsDB");
-    const appsCollection = database.collection("apps");
-
-    app.get("/apps", async (req, res) => {
-      try {
-        const apps = await appsCollection.find().toArray();
-        res.send(apps);
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
-      }
-    });
-
-    app.get("/app/:id", async (req, res) => {
-      try {
-        const appId = req.params.id;
-        if (id.length != 24) {
-          res.status(400).json({ error: "Invalid ID" });
-
-          return;
-        }
-        const query = new ObjectId(appId);
-        const app = await appsCollection.findOne({ _id: query });
-        res.json(app);
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
-      }
-    });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    const apps = await appsCollection.find().toArray();
+    res.send(apps);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
-run();
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/apps/:id", async (req, res) => {
+  try {
+    const appId = req.params.id;
 
-const server = app.listen(PORT, () => {
-  console.log(`Hero Apps Server listening on port ${PORT}`);
+    if (appId.length != 24) {
+      res.status(400).json({ error: "Invalid ID" });
+      return;
+    }
+    const query = new ObjectId(appId);
+    const app = await appsCollection.findOne({ _id: query });
+    res.json(app);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.all(/.*/, (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: "API not found",
+  });
 });
 
 // Basic routes
